8fa752a58209d7190c1568c50ded939758270d3c
crvs <crvs@crvs.xyz>
2020-01-02 22:34:21 +0100

    add documentation and verbatim deployment
    Added the capability to deploy directories (and files) that are not processed
    by blog-make.
    Added a man page and corresponding installation.

--- example/posts/examplepost.md
+++ example/posts/examplepost.md
@@ -5,4 +5,4 @@ date: 2019-12-29
 modified: 2019-12-30	 
 ---
 
-this is an example post of my [blog](INSERTBASEURL/index.html) 
+this is an example post of my [blog](index.html) where I also include an important [file](files/afile.txt).
--- install.sh
+++ install.sh
@@ -1,20 +1,37 @@
 #!/bin/sh
 
 user=$USER
-echo $user
+
+if [ "$user" = "root" ]; then
+    group=wheel
+
+    mode=555
+    destination=/usr/local/bin
+
+    docmode=444
+    docdestination=/usr/local/man
+
+else
+    group=$user
+
+    mode=700
+    destination=$HOME/.local/bin
+
+    docmode=600
+    docdestination=$HOME/.local/man
+fi
 
 for f in src/*; do
     filename=${f#src/}
-    if [ "$user" = "root" ]; then
-        mode=555
-        group=wheel
-        destination=/usr/local/bin
-    else
-        mode=700
-        group=$user
-        destination=$HOME/.local/bin
-    fi
-    install -m $mode -o $user -g $group $f $destination/$filename
+    install -m $mode -o $user -g $group $f \
+        $destination/$filename
+done
+
+for d in doc/*; do
+    section=${d##*.}
+    filename=${d#doc/}
+    install -m $mode -o $user -g $group $d \
+        $docdestination/man${section}/$filename
 done
 
 # vi: et sts=4 ts=4 sw=4
